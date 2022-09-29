4f463fb2deb32bf50a0d5f7325190780970db134
EduCDA <113633183+EduCDA@users.noreply.github.com>
2022-09-29 09:53:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_Comparación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int b;
+            x = 8;
+            y = 6;
+            n = y * x;
+            Console.WriteLine(x * y)
+            if (n > 20)
+
+            { Console.WriteLine(n + "es mayor a 19"); }
+
+            else { Console.WriteLine(n + "es menor a 19"); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
